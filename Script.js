beeaa6383c14302a64b125fbb1abbf1157ca6c76
nuel232 <130178735+nuel232@users.noreply.github.com>
2023-10-08 21:53:16 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,32 +1,54 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const calculateButton = document.querySelector("button"); // Select the button element
-    const resultDiv = document.getElementById("result");
+function calculateSleepDebt() {
+    
+    
+    const sleepHours = {
+        monday:parseFloat(document.getElementById('monday').value),
+        tuesday: parseFloat(document.getElementById('tuesday').value),
+        wednesday: parseFloat(document.getElementById('wednesday').value),
+        thursday: parseFloat(document.getElementById('thursday').value),
+        friday: parseFloat(document.getElementById('friday').value),
+        saturday: parseFloat(document.getElementById('saturday').value),
+        sunday: parseFloat(document.getElementById('sunday').value)
+    }
+    if (sleepHours.sunday> 24) {
+        document.getElementById('result').textContent = "You have inputed a value of more than 24 hours ";
 
-    calculateButton.addEventListener("click", function () {
-        let totalSleep = 0;
+    }else if (sleepHours.monday > 24) {
+        document.getElementById('result').textContent = "You have inputted a value of more than 24 hours ";
 
-        // Get sleep hours for each day of the week
-        const daysOfWeek = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
-        for (const day of daysOfWeek) {
-            const sleepHours = parseFloat(document.getElementById(day).value);
-            if (!isNaN(sleepHours) && sleepHours >= 0 && sleepHours <= 24) {
-                totalSleep += sleepHours;
-            } else {
-                alert("Please enter a valid number between 0 and 24 for " + day + ".");
-                return;
-            }
-        }
+    }else if (sleepHours.tuesday > 24) {
+        document.getElementById('result').textContent = "You have inputed a value of more than 24 hours ";
 
-        // Calculate sleep debt
-        const idealSleepPerDay = 8;
-        const idealSleepPerWeek = idealSleepPerDay * 7;
-        const sleepDebt = idealSleepPerWeek - totalSleep;
+    }else if (sleepHours.wednesday > 24) {
+        document.getElementById('result').textContent = "You have inputed a value of more than 24 hours ";
 
-        // Display result
-        if (sleepDebt < 0) {
-            resultDiv.textContent = `You've had ${-sleepDebt} hours more sleep!`;
+    }else if (sleepHours.thursday > 24) {
+        document.getElementById('result').textContent = "You have inputed a value of more than 24 hours ";
+
+    }else if (sleepHours.friday > 24) {
+        document.getElementById('result').textContent = "You have inputed a value of more than 24 hours ";
+
+    }else if (sleepHours.saturday > 24) {
+        document.getElementById('result').textContent = "You have inputed a value of more than 24 hours ";
+    } else {
+        const totalSleep = Object.values(sleepHours).reduce((acc, curr) => acc + curr, 0);
+        const idealSleepHoursPerWeek = 7 * 8; // 8 hours of sleep per day for 7 days
+        const sleepDebt = idealSleepHoursPerWeek - totalSleep;
+    
+        // Check if total sleep hours exceed 24
+    
+        let result = '';
+    
+        if (sleepDebt === 0) {
+            result = 'You have no sleep debt. Great job!';
+        } else if (sleepDebt > 0) {
+            result = `You need ${sleepDebt} more hours of sleep this week.`;
         } else {
-            resultDiv.textContent = `You need ${sleepDebt} hours of sleep this week.`;
+            result = `You've had more sleep than required by ${-sleepDebt} hours this week.`;
         }
-    });
-});
+    
+        document.getElementById('result').textContent = result;
+        
+    }
+
+}
